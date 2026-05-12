7a8ba6ef427c09b1f139431480eeea43128110e8
Prinya <raemju@gmail.com>
2026-05-12 09:15:14 +0700

    fix(rae): remove duplicate code in layout.tsx

--- app/rae/layout.tsx
+++ app/rae/layout.tsx
@@ -24,30 +24,4 @@ export default function RAERouteLayout({
       </div>
     </LanguageProvider>
   );
-"use client";
-
-import { LanguageProvider } from "@/components/rae/hooks/useLanguage";
-import RAEHeader from "@/components/rae/layout/RAEHeader";
-import RAEFooter from "@/components/rae/layout/RAEFooter";
-
-/**
- * RAE dedicated route layout at /rae.
- * Provides LanguageProvider context + sticky header + footer.
- */
-export default function RAERouteLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <LanguageProvider>
-      <div className="min-h-screen flex flex-col bg-white">
-        <RAEHeader />
-        <main id="main-content" className="flex-1">
-          {children}
-        </main>
-        <RAEFooter />
-      </div>
-    </LanguageProvider>
-  );
 }
